2835417ab2d8e4ac5f0301d2b839db3e10aa38c6
Alex Pshul <alex@pshul.com>
2019-05-29 00:09:56 +0300

    Simulated device code

--- SimulatedDevice/SimulatedDevice/Program.cs
+++ SimulatedDevice/SimulatedDevice/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
 
 namespace SimulatedDevice
 {
     class Program
     {
-        static void Main(string[] args)
+        private const int TakeSpot = 1;
+        private const int LeaveSpot = 2;
+
+        private const string DeviceConnectionString = @"";
+
+        private static readonly DeviceClient Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                int action = GetSimulatedAction();
+
+                var data = new { Action = action, ActionType = "SpotStateChange" };
+                string messageJson = JsonConvert.SerializeObject(data);
+                Message message = new Message(Encoding.ASCII.GetBytes(messageJson));
+
+                await Client.SendEventAsync(message);
+            }
         }
+
+        private static int GetSimulatedAction()
+        {
+            while (true)
+            { 
+                Console.Clear();
+                Console.WriteLine("Please select action number: ");
+                Console.WriteLine($"({TakeSpot}) Take Spot");
+                Console.WriteLine($"({LeaveSpot}) Leave Spot");
+
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (char.IsNumber(keyChar))
+                {
+                    int number = (int)char.GetNumericValue(keyChar);
+                    if (number == TakeSpot || number == LeaveSpot)
+                        return number;
+                }
+            }
+        }
+
+        
     }
 }
